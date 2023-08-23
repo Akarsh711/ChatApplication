402bc97c922424e46234e8c13b9d63b5f0a8d5d1
Akarsh711 <paliwalap7@gmail.com>
2023-08-23 22:00:12 +0530

    done with the vid but have a bug

--- ChatApplication/MVVM/ViewModel/MainViewModel.cs
+++ ChatApplication/MVVM/ViewModel/MainViewModel.cs
@@ -1,23 +1,59 @@
-﻿using ChatApplication.MVVM.Model;
+﻿using ChatApplication.Core;
+using ChatApplication.MVVM.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Security.Policy;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatApplication.MVVM.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel : ObservableObject
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
 
+
+        /* Commands */
+        public RelayCommand SendCommand { get; set; }
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set { _selectedContact = value; }
+        }
+        
+
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
         public MainViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+                Message = "";
+            });
 
             Messages.Add(new MessageModel
             {
@@ -29,31 +65,31 @@ namespace ChatApplication.MVVM.ViewModel
                 FirstMessage = true,
             });
 
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    Messages.Add(new MessageModel
-            //    {
-            //        Username = "Allision",
-            //        UsernameColor = "#409aff",
-            //        ImageSource = "https://i.imgur.com/yMWvLXd.png",
-            //        Message = "Test",
-            //        IsNativeOrigin = false,
-            //        FirstMessage = true,
-            //    });
-            //}
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Allision",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    IsNativeOrigin = false,
+                    FirstMessage = true,
+                });
+            }
 
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    Messages.Add(new MessageModel
-            //    {
-            //        Username = "Bunny",
-            //        UsernameColor = "#409aff",
-            //        ImageSource = "https://i.imgur.com/yMWvLXd.png",
-            //        Message = "Test",
-            //        IsNativeOrigin = false,
-            //        FirstMessage = true,
-            //    });
-            //}
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bunny",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    IsNativeOrigin = false,
+                    FirstMessage = true,
+                });
+            }
 
             Messages.Add(new MessageModel
             {
